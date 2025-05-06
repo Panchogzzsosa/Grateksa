34180ccd1cf6b51313f323032f6c755534224b8a
Panchogzzsosa <panchogzzsosa@gmail.com>
2025-05-05 19:14:59 -0600

    Cambios Finales

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
       { title: 'Capacitación de Brigadas', icon: '👥', desc: 'Entrenamiento práctico para equipos de emergencia.', img: '/images/servicios/servicio3.jpg' },
       { title: 'Protección civil en tu escuela', icon: '🚨', desc: 'Fomentar la cultura de la autoprotección en los estudiantes.', img: '/images/servicios/servicio1.jpg' },
       { title: 'Seguridad en eventos', icon: '📊', desc: 'Identificación y evaluación profesional de riesgos en eventos.', img: '/images/servicios/servicio2.jpg' },
-      { title: 'Programas Internos', icon: '📝', desc: 'Implementación de protocolos y simulacros.', img: '/images/servicios/servicio4.jpg' },
+      { title: 'Planes de Contingencia y Programas Internos de Protección Civil', icon: '📝', desc: 'Implementación de protocolos y simulacros.', img: '/images/servicios/servicio4.jpg' },
       { title: 'Bomberos y Fuerza Civil', icon: '🚒', desc: 'Atención profesional para empresas privadas.', img: '/images/servicios/servicio5.jpg' },
       { title: 'Ambulancias para Eventos', icon: '🚑', desc: 'Cobertura médica profesional para tu evento.', img: '/images/servicios/servicio6.jpg' }
     ]
