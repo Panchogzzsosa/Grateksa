b5952aac93df861cba084428b52e2cd5e2d45d0a
Panchogzzsosa <panchogzzsosa@gmail.com>
2025-05-06 16:28:59 -0600

    Cambios

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
       { title: 'Protección civil en tu escuela', icon: '🚨', desc: 'Fomentar la cultura de la autoprotección en los estudiantes.', img: '/images/servicios/servicio1.jpg' },
       { title: 'Seguridad en eventos', icon: '📊', desc: 'Identificación y evaluación profesional de riesgos en eventos.', img: '/images/servicios/servicio2.jpg' },
       { title: 'Planes de Contingencia y Programas Internos de Protección Civil', icon: '📝', desc: 'Implementación de protocolos y simulacros.', img: '/images/servicios/servicio4.jpg' },
-      { title: 'Bomberos y Fuerza Civil', icon: '🚒', desc: 'Atención profesional para empresas privadas.', img: '/images/servicios/servicio5.jpg' },
+      { title: 'Bomberos y Protección Civil', icon: '🚒', desc: 'Atención profesional para empresas privadas.', img: '/images/servicios/servicio5.jpg' },
       { title: 'Ambulancias para Eventos', icon: '🚑', desc: 'Cobertura médica profesional para tu evento.', img: '/images/servicios/servicio6.jpg' }
     ]
   });
